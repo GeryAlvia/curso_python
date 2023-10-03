c16a7ac9ba912e1873929dc39947500a810c8905
GeryAlvia <gerard.alvia@campus.xaloc.org>
2023-10-03 10:45:26 +0200

    Create Ejercicio 13.py

---
+++ Ejercicios_iniciale/Ejercicio 13.py
@@ -0,0 +1,8 @@
+#13. Realiza un programa que, a partir introducir el lado de un cubo, presente por pantalla el área y para calcular el volumen utiliza el operador de exponente.
+
+lado=float(input("Introde la medida del cubo del lado: "))
+a=lado**2
+v=lado**3
+print("El área del cubo es: ",a)
+print("El volumen del cubo es: ",v)
+input()
