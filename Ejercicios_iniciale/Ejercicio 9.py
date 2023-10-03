0fb56641595be298b6c51386d4fa18265b9eea43
GeryAlvia <gerard.alvia@campus.xaloc.org>
2023-10-03 10:46:32 +0200

    Create Ejercicio 9.py

---
+++ Ejercicios_iniciale/Ejercicio 9.py
@@ -0,0 +1,9 @@
+#9. programa que pida los segundos y muestre por pantalla y en la misma frase los minutos y las horas
+
+
+num1=(input("Introduce un numero"))
+min=float(num1)/60
+horas=float(num1)/3600
+print("El número de minutos es",min)
+print("El número en horas es",horas)
+input()
