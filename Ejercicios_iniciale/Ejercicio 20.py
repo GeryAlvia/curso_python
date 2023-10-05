3ddf17ec1118faa3ecf46e98c9059190de01d39f
GeryAlvia <gerard.alvia@campus.xaloc.org>
2023-10-05 09:55:51 +0200

    Update Ejercicio 20.py

--- Ejercicios_iniciale/Ejercicio 20.py
+++ Ejercicios_iniciale/Ejercicio 20.py
@@ -3,12 +3,9 @@
 var1=int(input("Introduzca un valor entre el 0 y el 10"))
 var2=int(input("Introduzca un valor entre el 0 y el 10"))
 
-if var1<var2: var1>var2 and var1>0 and var1<10 and var2>0 and var2<10: print("El número",var2,"es mayor que",var1,".")      
-elif var1>var2 and var1>0 and var1<10 and var2>0 and var2<10:
-    print("El número",var1,"es mayor que",var2,".")
-elif  var1>var2 and var1==0 and var1<10 and var2>0 and var2<10:
-    print("El número",var1,"es mayor que",var2,".")          
+if var1<var2: var1>var2 and var1>0 and var1<10 and var2>0 and var2<10
+
 
 else:
     print("Uno o los dos números están fuera de los límites establecidos")
-    input()
+input()
