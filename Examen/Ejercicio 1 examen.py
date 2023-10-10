ea3834b960fd968a9cc4f104cfe32431ade37054
GeryAlvia <gerard.alvia@campus.xaloc.org>
2023-10-10 10:41:42 +0200

    Create Ejercicio 1 examen.py

---
+++ Examen/Ejercicio 1 examen.py
@@ -0,0 +1,5 @@
+var1=float(input("Introduce un primer número: "))
+var2=float(input("Introduce un segundo número: "))
+var_total=var1+var2
+print("El resultado de sumar", var1,"y" , var2,"es: ",var_total)
+input()
