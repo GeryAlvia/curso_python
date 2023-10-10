d4533990515aa016e449ace9564a4876391ab2fa
GeryAlvia <gerard.alvia@campus.xaloc.org>
2023-10-10 10:42:07 +0200

    Create Ejercicio 0.py

---
+++ Ejercicios_iniciales/Ejercicio 0.py
@@ -0,0 +1,24 @@
+print ("MENÚ")
+print ("1. Operación a)")
+print ("2. Operación b)")
+print ("3. Operación c)")
+print ("4. Operación a)")
+
+var_opción=int(input("Introduce una opción del menú:"))
+
+if var_opción==1:
+    #codigo
+    print("codigo1")
+elif var_opción==2:
+    #codigo
+    print("codigo2")
+elif var_opción==3:
+    #codigo
+    print("codigo3")
+elif var_opción==4:
+    #codigo
+    print("codigo4")
+else:
+    print("Esta opción no está completa en el menú")
+
+    
