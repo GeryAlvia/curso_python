27d71b36620e15e85fb77c95cf5932f57d1f32c1
GeryAlvia <gerard.alvia@campus.xaloc.org>
2023-10-10 10:41:26 +0200

    Create Ejercicio 7.py

---
+++ Ejercicios_iniciales/Ejercicio 7.py
@@ -0,0 +1,16 @@
+#7. programa que calcule dos operandos con los 7 operadores vistos en clase. ¿Cómo puedes forzar que el resultado de la división tenga 2 decimales?
+Var1=int(input("Introduce el primer número"))
+Var2=int(input("Introduce el segundo número"))
+suma=Var1+Var2
+resta=Var1-Var2
+multi=Var1*Var2
+divi=Var1/Var2
+Expo=Var1**Var2
+divienter=Var1//Var2
+print ("La suma de operador1 y operador2 es:" ,suma)
+print("La resta de operador1 y operador2 es:" ,resta)
+print("La multiplicación de operador1 y operador2 es:" ,multi)
+print("La división de operador1 y operador2 es:" ,divi)
+print("El exponente de operador1 y operador2 es:" ,Expo)
+print("La división entera de operador1 y operador2:" ,divienter)     
+    
