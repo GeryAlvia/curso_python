78e803bd0518360a9f9b48914603a6db5ef29239
GeryAlvia <gerard.alvia@campus.xaloc.org>
2023-10-10 10:40:07 +0200

    Delete Ejercicio 4.py

--- Ejercicios_iniciale/Ejercicio 4.py
+++
@@ -1,7 +0,0 @@
-#4. A partir del código anterior, realiza una versión para números con decimales
-
-num1=float (input ("Introduce un número"))
-num2= float (input ("Introduce un segundo número"))
-total=num1 + num2
-print("El resultado de la suma es",total)
-input()
