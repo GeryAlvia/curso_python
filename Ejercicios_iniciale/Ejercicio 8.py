ac3ce6445f4bc183f88719405ab65214e9c29225
GeryAlvia <gerard.alvia@campus.xaloc.org>
2023-10-10 10:40:48 +0200

    Delete Ejercicio 8.py

--- Ejercicios_iniciale/Ejercicio 8.py
+++
@@ -1,9 +0,0 @@
-#8. programa que pida un número de horas y muestre por pantalla los minutos y segundos
-
-
-num1=(input("Introduce un numero"))
-min=int(num1)*60
-seg=int(num1)*3600
-print("El número de minutos es",min)
-print("El número en segundo es",seg)
-input()
