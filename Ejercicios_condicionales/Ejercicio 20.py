fe1c0ba1b872bcdb7e0693a2ef24aefee364bd07
GeryAlvia <gerard.alvia@campus.xaloc.org>
2023-10-10 10:38:30 +0200

    Create Ejercicio 20.py

---
+++ Ejercicios_condicionales/Ejercicio 20.py
@@ -0,0 +1,11 @@
+#20. A partir del ejercicio anterior, forzar que el usuario solo pueda introducir por teclados números entre 0 y 10
+
+var1=int(input("Introduzca un valor entre el 0 y el 10"))
+var2=int(input("Introduzca un valor entre el 0 y el 10"))
+
+if var1<var2: var1>var2 and var1>0 and var1<10 and var2>0 and var2<10
+
+
+else:
+    print("Uno o los dos números están fuera de los límites establecidos")
+input()
