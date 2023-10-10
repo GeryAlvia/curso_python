8091b0ff258a413be5d09a0c1ccd10ee1500198a
GeryAlvia <gerard.alvia@campus.xaloc.org>
2023-10-10 10:39:56 +0200

    Delete Ejercicio 3.py

--- Ejercicios_iniciale/Ejercicio 3.py
+++
@@ -1,8 +0,0 @@
-#3. Programa que pida dos números enteros y realice la suma correspondiente
-
-num1=int (input ("Introduce un número"))
-num2= int (input ("Introduce un segundo número"))
-total=num1 + num2
-print("El resultado de la suma es",total)
-input()
-
