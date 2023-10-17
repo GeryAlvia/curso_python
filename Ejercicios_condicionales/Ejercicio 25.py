45086b7fda9773d619588b05d3d9d47e8f3c878d
GeryAlvia <gerard.alvia@campus.xaloc.org>
2023-10-17 10:36:46 +0200

    Update Ejercicio 25.py

--- Ejercicios_condicionales/Ejercicio 25.py
+++ Ejercicios_condicionales/Ejercicio 25.py
@@ -1,12 +1,12 @@
 #25. Repite el programa número 23 pero en esta ocasión utilizando operadores lógicos.
 nota = float(input("Introduce la nota: "))
-if nota >= 8 and nota <= 10:
+if nota >=8 and nota <=11:
         print(f"La nota es un {nota} Excelente")    
-elif nota >= 6 and nota < 8:
+elif nota >=6.5 and nota <=8:
         print (f"La nota es un {nota} Notable")
-elif nota >= 5 and nota < 6:
+elif nota >=5 and nota <6:
         print(f"La nota es un {nota} Satisfactorio")
-elif nota < 5 and nota>0:
+elif nota <5 and nota>-1:
         print(f"La nota es un {nota} Insuficiente")
 else:
     print("La nota esta fuera de los parametros")
