b87aea65c074f16c1e14db32850e564767924e20
GeryAlvia <gerard.alvia@campus.xaloc.org>
2023-10-17 10:05:53 +0200

    Update Ejercicio 19.py

--- Ejercicios_condicionales/Ejercicio 19.py
+++ Ejercicios_condicionales/Ejercicio 19.py
@@ -1,17 +1,14 @@
 #19. Programa que introduzca dos números y devuelva cuál de los dos es mayor, menor o son iguales
 
+numero_1=int(input("Introduzca un numero: "))
+numero_2=int(input("Introduzca un numero: "))
+if numero_1<numero_2:
+    print(f"El numero {numero_1} es menor que el numero {numero_2}")
 
-var1=int(input("Introduce un número"))
-var2=int(input("Introduce un segundo número"))
-if var1>var2:
-            print("El número",var1,"es mayor que",var2,".")
+if numero_1>numero_2:
+    print(f"El numero {numero_1} es mayor que el numero {numero_2}")
 
-elif var2>var1:
-            print("El número",var2,"es mayor que",var1, ".")
-else:
-            print("Ambos números",var1,"y",var2,"son iguales")
-               
-                   
-
-               
-        
+if numero_1==numero_2:
+    print("Ambos numeros son iguales")
+input()
+          
