cf34efe4209f78dd9b31ce0bdae50668da93ae3c
GeryAlvia <gerard.alvia@campus.xaloc.org>
2023-10-24 10:02:07 +0200

    Update Ejercicio 26.py

--- Ejercicios_condicionales/Ejercicio 26.py
+++ Ejercicios_condicionales/Ejercicio 26.py
@@ -1,8 +1,11 @@
 #26. Realiza un programa que, al introducir una letra por teclado, aparezca por pantalla si está o no en mayúscula. Utiliza dos IF’s que establezcan True o False a la condición
 
-var11=input("Introduce una letra, mayuscula o minuscula: ")
-mayuscula=var1.isupper
-if mayuscula is True:
-    print("Es mayuscula")
-else mayuscula is False:
-    print("Es minuscula")
+letra = input("Introduce una letra: ")
+
+if letra.isalpha():  
+    if letra.isupper():  
+        print("La letra es mayúscula.")
+    else:
+        print("La letra es minúscula.")
+else:
+    print("La letra es mayúscula ¿?.")
