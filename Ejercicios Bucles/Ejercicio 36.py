45bfa9dffe9bb95b8bf9ac8f7243f4c5efd11671
GeryAlvia <gerard.alvia@campus.xaloc.org>
2023-11-23 09:44:11 +0100

    Update Ejercicio 36.py

--- Ejercicios Bucles/Ejercicio 36.py
+++ Ejercicios Bucles/Ejercicio 36.py
@@ -3,4 +3,4 @@ n = int(input("Introduce un número entero positivo: "))
 suma = 0
 for i in range(1, n + 1):
     suma += i
-print(f"La suma total de números naturales es: {suma}")
+print(f"La suma total de números naturales es: ",suma,"")
