68e497df1d5df2b35988736d3de510e0b06221d7
GeryAlvia <gerard.alvia@campus.xaloc.org>
2023-11-23 09:44:58 +0100

    Update Ejercicio 40.py

--- Ejercicios Bucles/Ejercicio 40.py
+++ Ejercicios Bucles/Ejercicio 40.py
@@ -2,13 +2,11 @@
 numero = 1
 total_pares = 0
 total_impares = 0
-
 while numero <= 50:
-    if numero % 2 == 0:
+     if numero % 2 == 0:
         total_pares += 1
-    else:
+else:
         total_impares += 1
-    numero += 1
-
+numero += 1
 print(f"El total de pares es: {total_pares}")
 print(f"El total de impares es: {total_impares}")
