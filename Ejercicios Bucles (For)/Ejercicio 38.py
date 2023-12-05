6b64401fb2be756562c42c0bb6f89a16e9e46224
GeryAlvia <gerard.alvia@campus.xaloc.org>
2023-12-05 10:15:24 +0100

    Create Ejercicio 38.py

---
+++ Ejercicios Bucles (For)/Ejercicio 38.py
@@ -0,0 +1,19 @@
+#38. A partir del programa anterior, establece los rangos para que el usuario no pueda introducir notas inferiores a 0 y superiores a 10
+num_notas = int(input("Introduce el número de notas que deseas introducir: "))
+
+for _ in range(num_notas):
+    while True:
+        try:
+            nota = float(input("Introduce la nota: "))
+            if 0 <= nota <= 10:
+                break  
+            else:
+                print("Error:   Has introducido la nota equivocada")
+        except ValueError:
+            print("Error: Ingresa un número válido.")
+
+    if nota >= 5:
+        print("Asignatura aprobada.")
+    else:
+        print("Asignatura suspendida.")
+input()
