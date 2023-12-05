e33a1d8c0dd741633feb2263dd91a5f14355f922
GeryAlvia <gerard.alvia@campus.xaloc.org>
2023-12-05 10:14:01 +0100

    Create Ejercicio 52.py

---
+++ Ejercicios Bucles (While)/Ejercicio 52.py
@@ -0,0 +1,10 @@
+#52. Realiza un programa que sume dos números enteros por teclado y presente el resultado por pantalla. El programa preguntará si deseas o no repetir la operación. Con While
+repetir = 's' 
+while repetir.lower() == 's': 
+    var1 = int(input("Introduce el primer número: "))
+    var2 = int(input("Introduce el segundo número: "))
+    suma = var1 + var2
+    print(f"El resultado de la suma es: ",suma,"")
+    repetir = input("¿Desea repetir la operación? (s/n): ")
+n=print("Mensaje: “Programa finalizado”")
+input()
