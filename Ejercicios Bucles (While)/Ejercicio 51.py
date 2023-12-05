ef32520daeb84fd15765cff112421379bb9b1950
GeryAlvia <gerard.alvia@campus.xaloc.org>
2023-12-05 10:20:26 +0100

    Delete Ejercicio 51.py

--- Ejercicios Bucles (While)/Ejercicio 51.py
+++
@@ -1,6 +0,0 @@
-#51. A partir del programa anterior, modifica el código para que sea el usuario quién introduzca el número de veces que desea que repita la frase Buenos días. Con While
-num_repeticiones = int(input("Ingrese el número de veces que desea repetir la frase 'Buenos días': "))
-a = 0 
-while a < num_repeticiones:
-    print("Buenos días")
-    a += 1  
