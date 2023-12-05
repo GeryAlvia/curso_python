128ed8c993eccf71f93447d6817e182bb3a3b2c5
GeryAlvia <gerard.alvia@campus.xaloc.org>
2023-12-05 10:19:45 +0100

    Delete Ejercicio 36.py

--- Ejercicios Bucles (For)/Ejercicio 36.py
+++
@@ -1,6 +0,0 @@
-#36. Programa que sume los n primeros números naturales. n Lo introduce el usuario.
-n = int(input("Introduce un número entero positivo: "))
-suma = 0
-for i in range(1, n + 1):
-    suma += i
-print(f"La suma total de números naturales es: ",suma,"")
