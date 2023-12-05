83834e8e108ec0614ae175154d8083c29ea46656
GeryAlvia <gerard.alvia@campus.xaloc.org>
2023-12-05 10:13:56 +0100

    Delete Ejercicio 35.py

--- Ejercicios Bucles/Ejercicio 35.py
+++
@@ -1,4 +0,0 @@
-#35. Programa que al introducir un número por teclado permita mostrar ese número de veces tu nombre
-numero = int(input("Introduce un número: "))
-for _ in range(numero):
-    print("Gerard Alvia")
