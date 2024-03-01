620bfe8158796e92b093bed1e930825861484e44
GeryAlvia <gerard.alvia@campus.xaloc.org>
2024-03-01 09:29:29 +0100

    Update Ejercicio 71.py

--- Ejercicios_Listas/Ejercicio 71.py
+++ Ejercicios_Listas/Ejercicio 71.py
@@ -1,15 +1,16 @@
 #71. Haz un programa que permita al usuario introducir letras en una lista (cantidad indefinida), en esta lista no deben almacenarse las letras que se han introducido repetidas.
-def main():
-    palabras = input("Introduce las palabras separadas por espacio: ").split()
-    lista1 = palabras[:]
-    lista2 = palabras[:]
-    lista1.sort()
-    lista2.sort(reverse=True)
-    print("Contenidos de lista1 en orden ascendente: ")
-    for palabra in lista1:
-        print(palabra, end=" ")
-    print("Contenidos de lista2 en orden descendente: ")
-    for palabra in lista2:
-        print(palabra, end=" ")
-if __name__ == "__main__":
-    main()
+letras = []
+while True:
+    entrada = input("Introduce una letra: ")
+    if entrada.isalpha():  # Verifica si la entrada es una letra
+        letra = entrada.lower()  # Convertimos la letra a minúscula  
+        if letra not in letras:
+            letras.append(letra)
+        else:
+            print("¡La letra ya está en la lista!")
+    else:
+        print("Entrada inválida. Debes introducir una letra.")
+    repetir = input("¿Deseas repetir? (s/n): ")
+    if repetir.lower() != 's':
+        break
+print(letras)
