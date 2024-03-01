04d3b94421d3b6cef7e619d8b4fd27db5811d3a4
GeryAlvia <gerard.alvia@campus.xaloc.org>
2024-03-01 09:29:13 +0100

    Create Ejercicio 73.py

---
+++ Ejercicios_Listas/Ejercicio 73.py
@@ -0,0 +1,20 @@
+
+def comprobar_repetidos(lista1, lista2):
+    repetidos = []
+    no_repetidos = []
+
+    for palabra in lista1:
+        if palabra in lista2:
+            repetidos.append(palabra)
+        else:
+            no_repetidos.append(palabra)
+
+    return repetidos, no_repetidos
+
+lista1 = ["casa", "mesa", "sal", "sol", "agua", "tres", "tren", "luz"]
+lista2 = ["casa", "sol", "pan"]
+
+repetidos, no_repetidos = comprobar_repetidos(lista1, lista2)
+
+print("Palabras repetidas:", repetidos)
+print("Palabras no repetidas:", no_repetidos)
